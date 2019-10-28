26b76ad2a0a6f0aaa70243f36d5b9f31fa9d9208
Ryan <Okoliryan50@gmail.com>
2019-10-28 16:13:22 +0100

    Added copy

--- App/components/Examples.jsx
+++ App/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
     render (){
         return(
+            <div>
             <h3>Examples Component!</h3>
+                <p>Welcome</p>
+            </div>
         )
     }
 });
